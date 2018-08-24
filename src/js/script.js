a50b568be15813812b647483839e078105818849
LucasWillems <your_email@example.com>
2018-08-24 13:45:51 +0200

    webgl done

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,22 @@
 const ml5 = require(`ml5`);
+const PIXI = require(`pixi.js`);
+const p2 = require(`p2`);
 
 const video = document.getElementById(`video`);
 const result = document.querySelector(`.result`);
+const resultcanvas = document.querySelector(`.resultcanvas`);
 
-import vertexSource from './lib/vertex-source.js';
-import fragmentSource from './lib/fragment-source.js';
+
+// import vertexSource from './lib/vertex-source.js';
+// import fragmentSource from './lib/fragment-source.js';
+
+const app = new PIXI.Application({
+  width: window.innerWidth / 2, height: 500});
+
+
+let world, boxShape, boxBody, planeShape, planeBody, rect, currentWord;
+
+const allWords = [];
 
 const recogniseMe = () => {
   navigator.mediaDevices.getUserMedia({video: true})
@@ -25,84 +37,152 @@ const loop = classifier => {
   classifier.predict()
     .then(results => {
       const res = results[0].className.split(`, `);
-      result.innerText = res[0];
+      currentWord = res[0];
+      result.innerText = currentWord;
     });
 };
 
-const createShader = (gl, type, source) => {
-  const shader = gl.createShader(type);
-  gl.shaderSource(shader, source);
-  gl.compileShader(shader);
-  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-  if (success) {
-    return shader;
+const addWord = () => {
+  if (currentWord == null) {
+    console.log(`nog niets herkent`);
+  } else {
+    if (allWords.indexOf(currentWord) === - 1) {
+      allWords.push(currentWord);
+      console.log(allWords);
+      addBox();
+      addGraphics(currentWord);
+    } else {
+      console.log(`already in`);
+    }
   }
-
-  console.log(gl.getShaderInfoLog(shader));
-  gl.deleteShader(shader);
 };
 
-const createProgram = (gl, vertexShader, fragmentShader) => {
-  const program = gl.createProgram();
-  gl.attachShader(program, vertexShader);
-  gl.attachShader(program, fragmentShader);
-  gl.linkProgram(program);
-  const success = gl.getProgramParameter(program, gl.LINK_STATUS);
-  if (success) {
-    return program;
-  }
+const startp2 = () => {
+  world = new p2.World();
 
-  console.log(gl.getProgramInfoLog(program));
-  gl.deleteProgram(program);
+  planeShape = new p2.Plane();
+  planeBody = new p2.Body({position: [ 0, - 250],
+    color: 0xFFFFFF});
+
+  const wallShapeLeft = new p2.Plane();
+  const wallBodyLeft = new p2.Body({
+    position: [ - 300, 0],
+    angle: - Math.PI / 2
+  });
+
+
+  // const wallShapeRight = new p2.Plane();
+  // const wallBodyRight = new p2.Body({position: [400, 400]});
+
+
+
+  planeBody.addShape(planeShape);
+  world.addBody(planeBody);
+
+  wallBodyLeft.addShape(wallShapeLeft);
+  world.addBody(wallBodyLeft);
+  //
+  // wallBodyRight.addShape(wallShapeRight);
+  // world.addBody(wallBodyRight);
+
+  startPixi();
+
+  const addword = document.querySelector(`.addword`);
+  addword.addEventListener(`click`, addWord);
+
 };
 
-const startGL = () => {
-  const canvas = document.querySelector(`.resultcanvas`),
-    gl = canvas.getContext(`webgl`);
+const addBox = () => {
+  boxShape = new p2.Box({width: 100, height: 50});
+  boxBody = new p2.Body({
+    mass: 1000,
+    // position: [0, 400],
+    position: [Math.random() * (300 - - 300) - 300, 400],
+    angularVelocity: 1,
+    height: resultcanvas.innerHeight
+  });
+  boxBody.addShape(boxShape);
+  world.addBody(boxBody);
 
-  // const vertexSource = document.getElementById(`vertex-source`).text;
-  const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexSource);
+};
 
-  // const fragmentSource = document.getElementById(`fragment-source`).text;
-  const fragmentShader = createShader(gl, gl.FRAGMENT_SHADER, fragmentSource);
+const startPixi = () => {
+  // zoom = 100;
 
-  const program = createProgram(gl, vertexShader, fragmentShader);
 
-  const positionAttribute = gl.getAttribLocation(program, `a_position`);
-  const positionBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  // three 2d points
-  const positions = new Float32Array([
-    0.5, 0.5,
-    0, 0.5,
-    0.5, 0,
+  resultcanvas.appendChild(app.view);
 
-    0, 0,
-    0, 0.5,
-    0.5, 0
-  ]);
-  gl.bufferData(gl.ARRAY_BUFFER, positions, gl.STATIC_DRAW);
+  console.log(resultcanvas);
 
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-  // Clear the canvas
-  gl.clearColor(0, 1, 0, 1);
-  gl.clear(gl.COLOR_BUFFER_BIT);
+  app.renderer.backgroundColor = 0x488968;
 
-  // Tell it to use our program (pair of shaders)
-  gl.useProgram(program);
-  gl.enableVertexAttribArray(positionAttribute);
+    // Add transform to the container
+  app.stage.position.x =  app.renderer.width / 2;
+  app.stage.position.y =  app.renderer.height / 2;
+  // app.stage.scale.x =  zoom;
+  app.stage.scale.y =  - 1;
 
-  // Bind the position buffer.
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  gl.vertexAttribPointer(positionAttribute, 2, gl.FLOAT, false, 0, 0);
+  console.log(app.renderer.width);
+};
 
-  gl.drawArrays(gl.TRIANGLES, 0, 6);
+const addGraphics = () => {
+  console.log(currentWord);
+  rect = new PIXI.Graphics();
+  rect.beginFill(0x107757);
+  rect.drawRect(- boxShape.width / 2, - boxShape.height / 2, boxShape.width, boxShape.height);
+  const text = new PIXI.Text(`${currentWord}`, {
+    fontFamily: `Arial`,
+    fontSize: 14,
+    fill: 0xFFFFFF,
+  });
+
+  text.position.x = - 50;
+
+  text.scale.y = - 1;
+
+
+  rect.interactive = true;
+  rect.buttonMode = true;
+
+  rect.customProperty = currentWord;
+
+  rect.on(`pointerdown`, onBtnClick);
+
+
+
+
+  rect.addChild(text);
+  app.stage.addChild(rect);
+
+};
+
+const onBtnClick = e => {
+  console.log(e.target.customProperty);
+};
+
+const animate = () => {
+  requestAnimationFrame(animate);
+  if (allWords.length !== 0) {
+    world.step(1 / 10);
+
+    rect.position.x = boxBody.position[0];
+    rect.position.y = boxBody.position[1];
+    rect.rotation = boxBody.angle;
+
+    app.renderer.render(app.stage);
+
+  }
 };
 
 const init = () => {
   recogniseMe();
 
-  startGL();
+  startp2();
+  animate();
+
 };
+
+
+
 
 window.onload = init;
